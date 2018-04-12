bd7e4996c72217c695808b1e5b4930c326495a1b
Wolfgang Hochleitner <wolfgang@hochleitner.cc>
2018-04-12 13:24:47 +0200

    Updated documentation
    Generate separate versions for master and v1.0.0

--- samiconfig.php
+++ samiconfig.php
@@ -15,14 +15,14 @@ $versions = \Sami\Version\GitVersionCollection::create($dir)
     ->addFromTags("v1.0.*")
     ->add("master", "Development master");
 
-$sami = new Sami($iterator, array(
+$sami = new Sami($iterator, [
     "title" => "NormForm API",
     "versions" => $versions,
     "build_dir" => __DIR__ . "/docs/%version%",
     "cache_dir" => __DIR__ . "/cache/%version%",
     "remote_repository" => new GitHubRemoteRepository("Digital-Media/normform", __DIR__),
     "default_opened_level" => 2
-));
+]);
 
 // Also include private properties and methods
 $sami["filter"] = function () {
